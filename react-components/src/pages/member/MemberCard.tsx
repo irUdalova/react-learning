55fe75f6516ef3a1081d024e934d691e0ef69840
irUdalova <irinabondarenko.mail@gmail.com>
2022-11-19 21:10:06 +0200

    feat: rewrite the form with React Hook Form

--- react-components/src/pages/member/MemberCard.tsx
+++ react-components/src/pages/member/MemberCard.tsx
@@ -6,40 +6,38 @@ type CardsValues = {
   cardData: CardType;
 };
 
-export class MemberCard extends React.Component<CardsValues> {
-  render() {
-    const { url, name, date, country, isAgreeTerms, isAgreePromo } = this.props.cardData;
-    return (
-      <>
-        <div className="member-card">
-          <div className="avatar-img">
-            <img
-              className="avatar-img__uploaded"
-              width={90}
-              height={90}
-              src={url || 'assets/svg/user.svg'}
-            />
-          </div>
-
-          <div className="name">{name || 'Unknown'}</div>
-
-          <div className="date">{date || 'Unknown'}</div>
-
-          <div className="country">{country}</div>
-
-          <div className="terms">
-            {isAgreeTerms
-              ? 'I agree to the terms and conditions'
-              : "I don't agree to the terms and conditions"}
-          </div>
-
-          <div className="promo">
-            {isAgreePromo
-              ? ' I want to receive notifications about promo'
-              : 'I want to receive notifications about promo'}
-          </div>
+export function MemberCard({ cardData }: CardsValues) {
+  const { url, name, date, country, isAgreeTerms, isAgreePromo } = cardData;
+  return (
+    <>
+      <div className="member-card">
+        <div className="avatar-img">
+          <img
+            className="avatar-img__uploaded"
+            width={90}
+            height={90}
+            src={url || 'assets/svg/user.svg'}
+          />
         </div>
-      </>
-    );
-  }
+
+        <div className="name">{name || 'Unknown'}</div>
+
+        <div className="date">{date || 'Unknown'}</div>
+
+        <div className="country">{country}</div>
+
+        <div className="terms">
+          {isAgreeTerms
+            ? 'I agree to the terms and conditions'
+            : "I don't agree to the terms and conditions"}
+        </div>
+
+        <div className="promo">
+          {isAgreePromo
+            ? ' I want to receive notifications about promo'
+            : 'I want to receive notifications about promo'}
+        </div>
+      </div>
+    </>
+  );
 }
--- react-components/src/pages/member/MemberForm.tsx
+++ react-components/src/pages/member/MemberForm.tsx
@@ -1,283 +1,174 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { CardType } from 'types';
 import './Member.css';
 
 type PropsMemberType = {
-  onSubmit: (cardData: CardType) => void;
+  onSubmitForm: (cardData: CardType) => void;
 };
 
-type ErrorsType = {
-  nameError: boolean;
-  dateError: boolean;
-  countryError: boolean;
-  agreeError: boolean;
+type FormDataType = {
+  userName: string;
+  userDate: string;
+  userCountry: string;
+  userAgree: boolean;
+  userPromo: boolean;
 };
 
-type StateMemberType = {
-  url: string;
-  errors: ErrorsType;
-  submitSuccess: boolean;
-};
+export function MemberForm({ onSubmitForm }: PropsMemberType) {
+  const [urlImg, setUrlImg] = useState('');
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormDataType>({
+    defaultValues: {
+      userAgree: false,
+    },
+  });
 
-export class MemberForm extends React.Component<PropsMemberType, StateMemberType> {
-  private checkRef: React.RefObject<HTMLInputElement>;
-  constructor(props: PropsMemberType) {
-    super(props);
-    this.state = {
-      url: '',
-      errors: {
-        nameError: false,
-        dateError: false,
-        countryError: false,
-        agreeError: false,
-      },
-      submitSuccess: false,
-    };
-    this.checkRef = React.createRef();
-  }
-
-  handleChangeFile(file: Blob) {
+  const handleChangeFile = (file: Blob) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
       localStorage.setItem('avatar', reader.result as string);
-      this.setState({ ...this.state, url: reader.result as string });
+      setUrlImg(reader.result as string);
     };
-  }
+  };
 
-  get hasErrors() {
-    return Object.values(this.state.errors).some((er) => er);
-  }
-
-  onSubmit(event: React.FormEvent) {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement;
-    const cardData = this.validation(form);
-    // console.log('cardData', cardData);
+  const onSubmit = (data: FormDataType) => {
+    const cardData = {
+      id: Math.random(),
+      url: urlImg || '',
+      name: data.userName,
+      date: data.userDate,
+      country: data.userCountry,
+      isAgreeTerms: data.userAgree,
+      isAgreePromo: data.userPromo,
+    };
+    onSubmitForm(cardData);
 
     if (cardData) {
-      this.props.onSubmit(cardData);
-      this.setState({ ...this.state, submitSuccess: true });
-      setTimeout(this.reset, 2000, form);
-    }
-
-    // console.log('isSeccess', this.state.submitSuccess);
-  }
-
-  validation = (form: HTMLFormElement) => {
-    const formData = new FormData(form);
-    const userName = formData.get('userName') as string;
-    const userDate = formData.get('userDate') as string;
-    const userCountry = formData.get('userCountry') as string;
-    const userAgree = formData.get('userAgree');
-    const userPromo = formData.get('userPromo');
-
-    const errors: Partial<ErrorsType> = {};
-
-    if (!userName) {
-      errors.nameError = true;
-    }
-    if (!userDate) {
-      errors.dateError = true;
-    }
-    if (!userCountry) {
-      errors.countryError = true;
-    }
-    if (!userAgree) {
-      errors.agreeError = true;
-    }
-
-    if (Object.entries(errors).length) {
-      this.setState({
-        ...this.state,
-        errors: { ...this.state.errors, ...errors },
-      });
-      return;
-    }
-
-    return {
-      id: Math.random(),
-      url: this.state.url || '',
-      name: userName,
-      date: userDate,
-      country: userCountry,
-      isAgreeTerms: !!userAgree,
-      isAgreePromo: !!userPromo,
-    };
-  };
-
-  reset = (form: HTMLFormElement) => {
-    form.reset();
-    localStorage.removeItem('avatar');
-    this.setState({
-      ...this.state,
-      url: '',
-      errors: {
-        nameError: false,
-        dateError: false,
-        countryError: false,
-        agreeError: false,
-      },
-      submitSuccess: false,
-    });
-
-    if (this.checkRef.current !== null) {
-      this.checkRef.current.checked = false;
+      setSubmitSuccess(true);
+      setTimeout(resetForm, 2000);
     }
   };
 
-  render() {
-    return (
-      <>
-        {this.state.submitSuccess && (
-          <div className="submit-success">Your data has been successfully saved!</div>
-        )}
-        <form
-          className="form"
-          onSubmit={this.onSubmit.bind(this)}
-          id="form-member"
-          onChange={(e) => {
-            console.log('form change', e);
-          }}
-        >
-          <div className="avatar-field">
-            <div className="avatar-img">
-              {this.state.url && (
-                <img className="avatar-img__uploaded" width={90} height={90} src={this.state.url} />
-              )}
-              {!this.state.url && (
-                <img
-                  className="avatar-img__unknown"
-                  width={90}
-                  height={90}
-                  src="assets/svg/user.svg"
-                />
-              )}
-            </div>
-            <input
-              className="avatar-field__input"
-              type="file"
-              accept=".jpg, .jpeg, .png"
-              id="file"
-              onChange={(e) => {
-                if (!e.target.files) return;
-                this.handleChangeFile(e.target.files[0]);
-              }}
-            />
-            <label className="avatar-field__label" htmlFor="file">
-              Choose an image
-            </label>
-          </div>
+  const resetForm = () => {
+    setUrlImg('');
+    reset();
+    setSubmitSuccess(false);
+  };
 
-          <div className="name-field">
-            <label className="text-field__label" htmlFor="UserName">
-              Name
-            </label>
-            <input
-              className={`text-field__input ${
-                this.state.errors.nameError ? 'form__input_error' : ''
-              }`}
-              type="text"
-              id="userName"
-              name="userName"
-              placeholder="User Name"
-              onChange={() => {
-                if (this.state.errors.nameError) {
-                  this.setState({
-                    ...this.state,
-                    errors: { ...this.state.errors, nameError: false },
-                  });
-                }
-              }}
-            />
-            {this.state.errors.nameError && <p className="form__error">Please, enter your name!</p>}
-          </div>
-
-          <div className="date-field">
-            <label className="text-field__label" htmlFor="date">
-              Date of Birth
-            </label>
-            <input
-              className={`text-field__input ${
-                this.state.errors.dateError ? 'form__input_error' : ''
-              }`}
-              type="date"
-              id="date"
-              name="userDate"
-              onChange={() => {
-                this.setState({
-                  ...this.state,
-                  errors: { ...this.state.errors, dateError: false },
-                });
-              }}
-            />
-            {this.state.errors.dateError && (
-              <p className="form__error">Please, enter your date of birth!</p>
+  return (
+    <>
+      {submitSuccess && (
+        <div className="submit-success">Your data has been successfully saved!</div>
+      )}
+      <form className="form" onSubmit={handleSubmit(onSubmit)} id="form-member">
+        <div className="avatar-field">
+          <div className="avatar-img">
+            {urlImg && <img className="avatar-img__uploaded" width={90} height={90} src={urlImg} />}
+            {!urlImg && (
+              <img
+                className="avatar-img__unknown"
+                width={90}
+                height={90}
+                src="assets/svg/user.svg"
+              />
             )}
           </div>
+          <input
+            className="avatar-field__input"
+            type="file"
+            accept=".jpg, .jpeg, .png"
+            id="file"
+            onChange={(e) => {
+              if (!e.target.files) return;
+              handleChangeFile(e.target.files[0]);
+            }}
+          />
+          <label className="avatar-field__label" htmlFor="file">
+            Choose an image
+          </label>
+        </div>
 
-          <div className="select-field">
-            <p className="select__label">Countries</p>
-            <select
-              className={`select ${this.state.errors.countryError ? 'form__input_error' : ''} `}
-              name="userCountry"
-              defaultValue={''}
-              onChange={() => {
-                this.setState({
-                  ...this.state,
-                  errors: { ...this.state.errors, countryError: false },
-                });
-              }}
-            >
-              <option value="" disabled className="select__placeholder">
-                Select your option
-              </option>
-              <option value="Ukraine">Ukraine</option>
-              <option value="Poland">Poland</option>
-              <option value="France">France</option>
-              <option value="USA">USA</option>
-            </select>
-            {this.state.errors.countryError && (
-              <p className="form__error">Please, choose your country!</p>
-            )}
-          </div>
+        <div className="name-field">
+          <label className="text-field__label" htmlFor="UserName">
+            Name
+          </label>
+          <input
+            className={`text-field__input ${errors?.userName ? 'form__input_error' : ''}`}
+            type="text"
+            id="userName"
+            {...register('userName', { required: 'Please, enter your name!' })}
+            placeholder="User Name"
+          />
+          {errors?.userName && <p className="form__error">{`${errors?.userName?.message}`}</p>}
+        </div>
 
-          <div className="check-field">
-            <input
-              className="check-field__input"
-              type="checkbox"
-              id="userAgree"
-              ref={this.checkRef}
-              name="userAgree"
-              defaultChecked={false}
-              onChange={() => {
-                console.log('change');
+        <div className="date-field">
+          <label className="text-field__label" htmlFor="date">
+            Date of Birth
+          </label>
+          <input
+            className={`text-field__input ${errors.userDate ? 'form__input_error' : ''}`}
+            type="date"
+            id="date"
+            {...register('userDate', { required: 'Please, enter your date of birth!' })}
+          />
+          {errors?.userDate && <p className="form__error">{`${errors?.userDate?.message}`}</p>}
+        </div>
 
-                this.setState({
-                  ...this.state,
-                  errors: { ...this.state.errors, agreeError: false },
-                });
-              }}
-            />
-            <label className="check-field__label" htmlFor="userAgree">
-              I agree to the terms and conditions.
-            </label>
-            {this.state.errors.agreeError && (
-              <p className="agree__error">You must agree with the rules!</p>
-            )}
-          </div>
+        <div className="select-field">
+          <p className="select__label">Countries</p>
+          <select
+            className={`select ${errors.userCountry ? 'form__input_error' : ''} `}
+            {...register('userCountry', { required: 'Please, choose your country!' })}
+            defaultValue={''}
+          >
+            <option value="" disabled className="select__placeholder">
+              Select your option
+            </option>
+            <option value="Ukraine">Ukraine</option>
+            <option value="Poland">Poland</option>
+            <option value="France">France</option>
+            <option value="USA">USA</option>
+          </select>
+          {errors.userCountry && <p className="form__error">{`${errors?.userCountry?.message}`}</p>}
+        </div>
 
-          <div className="switch-field">
-            <label className="switch">
-              <input type="checkbox" name="userPromo" />
-              <span className="slider round"></span>
-            </label>
-            <p className="switch-field__text">I want to receive notifications about promo</p>
-          </div>
+        <div className="check-field">
+          <input
+            className="check-field__input"
+            type="checkbox"
+            id="userAgree"
+            {...register('userAgree', { required: 'You must agree with the rules!' })}
+            defaultChecked={false}
+          />
+          <label className="check-field__label" htmlFor="userAgree">
+            I agree to the terms and conditions.
+          </label>
+          {errors.userAgree && <p className="agree__error">{`${errors?.userAgree?.message}`}</p>}
+        </div>
 
-          <input className="submit-btn" type="submit" value="Create" disabled={this.hasErrors} />
-        </form>
-      </>
-    );
-  }
+        <div className="switch-field">
+          <label className="switch">
+            <input type="checkbox" {...register('userPromo')} />
+            <span className="slider round"></span>
+          </label>
+          <p className="switch-field__text">I want to receive notifications about promo</p>
+        </div>
+
+        <input
+          className="submit-btn"
+          type="submit"
+          value="Create"
+          disabled={!!Object.values(errors).length}
+        />
+      </form>
+    </>
+  );
 }
